d4e0302c23459f18a9b698c2625223c44ce29b3c
Devyn <kinisongaming@gmail.com>
2018-11-28 10:10:53 -0800

    Add files via upload
    Player code

---
+++ PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public enum type { platformer, topDown};
+    public type moveType;
+    public float moveSpeed, jumpHeight;
+    public bool onGround = false;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+    if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+        }
+    if (moveType == type.platformer && Input.GetAxisRaw("Vertical") >  0)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);  
+        }
+    }
+    void OnCollisionEnter2D (Collision2D obj)
+    {
+        if (obj.gameObject.tag == "ground") {
+            onGround = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "ground")
+        {
+            onGround = false;
+        }
+    }
+}
