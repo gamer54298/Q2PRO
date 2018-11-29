5ba7ea9d242692ce1d662641b4c8c8bef16e3825
Devyn <kinisongaming@gmail.com>
2018-11-29 09:38:34 -0800

    Add files via upload
    Added lives as an int

--- Health.cs
+++ Health.cs
@@ -6,13 +6,26 @@ public class Health : MonoBehaviour {
 
     public const int maxHealth = 100;
     public int currentHealth = maxHealth;
+    public int Lives;
     public void TakeDamage(int amount)
+    
     {
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
+
             currentHealth = 0;
             Debug.Log("Dead!");
+            Lives --;
+            
+        }
+    }
+    public void TakeLive(int amount)
+    {
+        if (Lives == 0)
+        {
+            
+            Debug.Log("No more Lives");
         }
     }
 }
