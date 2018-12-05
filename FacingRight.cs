d541aa59b94169b1b61f66603acfee21dfbaf27d
Devyn <kinisongaming@gmail.com>
2018-12-05 09:39:14 -0800

    Add files via upload
    Facing right

---
+++ FacingRight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+    private bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Flip ()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
